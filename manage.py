7d7030f13cde98bfe30c648fd6040de952aaf734
Takis <takis.panagiot1@gmail.com>
2021-01-05 22:44:34 +0200

    fourth commit

--- manage.py
+++ manage.py
@@ -23,33 +23,27 @@ def calculate_order_amount(items):
     # people from directly manipulating the amount on the client
 
     return 1400
-
+@app.route("/")
+def main():
+    return "test"
 @app.route('/create-payment-intent', methods=['POST'])
 
 def create_payment():
 
     try:
-
         data = json.loads(request.data)
-
         intent = stripe.PaymentIntent.create(
-
             amount=calculate_order_amount(data['items']),
-
             currency='euro'
-
         )
 
         return jsonify({
-
           'clientSecret': intent['client_secret']
 
         })
 
     except Exception as e:
-
         return jsonify(error=str(e)), 403
 
 if __name__ == '__main__':
-
     app.run()
